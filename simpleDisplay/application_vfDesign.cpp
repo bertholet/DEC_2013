dffc70bf4627e3a0f8a091243d58e93e333b24ce
Peter Bertholet <peter_bertholet@hotmail.com>
2013-02-05 18:11:49 +0100

    Vfield design: source sink flow implemented

--- simpleDisplay/application_vfDesign.cpp
+++ simpleDisplay/application_vfDesign.cpp
@@ -1,4 +1,5 @@
 #include "application_vfDesign.h"
+#include "meshMath.h"
 
 
 application_vfDesign::application_vfDesign(void)
@@ -19,11 +20,12 @@ void application_vfDesign::computeField( MODEL & model,
 	oneForm & target )
 {
   
-	cpuCSRMatrix mat = model.getBorder2()* model.getStar2() * model.getD1();
-	cpuCSRMatrix star0_inv = model.getStar0_mixed();
-	star0_inv.elementWiseInv();
 	cpuCSRMatrix duald1_border = model.getBorder1();
 	cpuCSRMatrix duald1_border_transposed = model.getD0();
+	cpuCSRMatrix star0_inv = model.getStar0_mixed();
+	star0_inv.elementWiseInv();
+
+	cpuCSRMatrix mat = model.getBorder2()* model.getStar2() * model.getD1();
 	mat = mat +(model.getStar1_mixed()*duald1_border_transposed * star0_inv* (duald1_border) *model.getStar1_mixed());
 
 	
@@ -31,17 +33,46 @@ void application_vfDesign::computeField( MODEL & model,
 //	std::vector<int> diag_ind;
 //	mat.getDiagonalIndices(diag_ind);
 	float weight = 100;
+	float srcsink_flow = 10;
 
 	for(int i = 0; i < constrainedEdges.size(); i++){
 		mat.add(constrainedEdges[i], constrainedEdges[i],weight);
 	}
 
+	//////////////////////////////////////////////////////////////////////////
 	//setup b
+	//////////////////////////////////////////////////////////////////////////
 	floatVector b(model.getMesh()->getEdges().size());
 
 	for(int i = 0; i < constrainedEdges.size(); i++){
 		b[constrainedEdges[i]]+=weight*edgeConstraints[i];
 	}
+
+	//lightweight implementation of star1 *d *source_flow for the source sink constraints
+	int vert;
+	for(int i =0; i < sink_verts.size(); i++){
+		vert =sink_verts[i];
+		wingedEdge* edg = & model.getMesh()->getAnEdge(vert);
+		wingedEdge* first= edg;
+		do{
+			b[edg->getIndex()] -= edg->orientation(vert) * srcsink_flow
+				*meshMath::dualEdge_edge_ratio(edg->getIndex(),* model.getMesh());
+			edg = & edg->getNext_bc(vert);
+		}
+		while(edg!=first);
+	}
+	for(int i =0; i < source_verts.size(); i++){
+		vert =source_verts[i];
+		wingedEdge* edg = & model.getMesh()->getAnEdge(vert);
+		wingedEdge* first= edg;
+		do{
+			b[edg->getIndex()] += edg->orientation(vert) * srcsink_flow
+				*meshMath::dualEdge_edge_ratio(edg->getIndex(),* model.getMesh());
+			edg = & edg->getNext_bc(vert);
+		}
+		while(edg!=first);
+	}
+
 
 	//////////////////////////////////////////////////////////////////////////
 	//solving.....
