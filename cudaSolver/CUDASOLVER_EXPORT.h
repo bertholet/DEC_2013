e5c80f57c23fbb09d3990f3529a1cdb9d44f95b3
Peter Bertholet <peter_bertholet@hotmail.com>
2013-04-16 17:50:10 +0200

    git cleaning: *cudaSolver Directory *.gitignore

--- cudaSolver/CUDASOLVER_EXPORT.h
+++
@@ -1,9 +0,0 @@
-#ifdef COMPILE_CUDASOLVER
-	#ifndef CUDASOLVER_EXPORT
-	#define CUDASOLVER_EXPORT __declspec(dllexport)
-	#endif
-#else
-	#ifndef CUDASOLVER_EXPORT
-	#define CUDASOLVER_EXPORT __declspec(dllimport)
-	#endif
-#endif
--- cudaSolver/kernel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CUDASOLVER_EXPORT.h"
-
-namespace CUDA_STUFF{
-
-int  CUDASOLVER_EXPORT main();
-
-}
