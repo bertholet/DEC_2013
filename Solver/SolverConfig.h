3b7cb99e02cd500b8514fe31172efbca3b242d6b
Peter Bertholet <peter_bertholet@hotmail.com>
2013-04-16 17:43:20 +0200

    Adding DEC folder to gitignore, adding comments to SolverConfig.h

--- Solver/SolverConfig.h
+++ Solver/SolverConfig.h
@@ -1,12 +1,16 @@
 /************************************************************************/
 /* Configure which Solver to use
-/* actually there are wrappers for two libraries implemented, the library files needed
-/* are not provided with this project, they need to be compiled/downloaded separately:
+/* At the moment there are wrappers for two libraries implemented. The library files needed
+/* are not provided with this project, they need to be compiled/downloaded separately.
+/* The two libraries are:
 /*
 /* 1. the Suitesparse Library, which is open source but a real pain to compile
 /* 2. The Solver from the Pardiso project, which is shareware with a 1-year academic
 /* license as well as a one-month trial license, and can be downloaded precompiled on
 /*
+/* Using the config file solvers can be created transparently using th
+/* syntax: new solverInstance(MatrixType type), which returns SolverIf *
+/* of the configured type.
 /************************************************************************/
 #pragma once
 
@@ -15,7 +19,6 @@
 //#define SUITESPARSESOLVER
 
 
-//syntax: new solverInstance(MatrixType type), returns SolverIf *.
 #ifdef PARDISOSOLVER
 #define solverInstance PardisoSolver  
 #include "PardisoSolver.h"
